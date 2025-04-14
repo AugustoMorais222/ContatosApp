ca84bc408139501f1302eb00267617ea476c80c0
AugustoMorais222 <augustodemor04@gmail.com>
2025-04-14 13:51:32 -0300

    Arrumando rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormularioContatosComponent } from "./formulario-contatos/formulario-contatos.component";
 import { ListaContatosComponent } from "./lista-contatos/lista-contatos.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormularioContatosComponent, ListaContatosComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RouterModule],
+  template: `
+  <h1>Cadastro App</h1>
+  <nav>
+    <a routerLink="/formulario">Formulário</a> |
+    <a routerLink="/list">Lista</a>
+  </nav>
+  <router-outlet></router-outlet>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListaContatosComponent } from './lista-contatos/lista-contatos.component';
+import { FormularioContatosComponent } from './formulario-contatos/formulario-contatos.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/formulario', pathMatch: 'full'},
+    { path: 'list', component: ListaContatosComponent},
+    { path: 'formulario', component: FormularioContatosComponent},
+];
